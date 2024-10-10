736661a500131b6321748f6739c3b0abd1f093fa
kurosan-5 <kurowassan55555@gmail.com>
2024-10-10 17:06:44 +0900

    主要テーブル作成

---
+++ database/migrations/2024_10_10_073550_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->string('about', 255);
+            $table->text('description');
+            $table->integer('like')->default(0);
+            $table->string('image_path');
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
